49c19857cece209ba33d91a08bd2fa545b793408
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-30 20:18:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let btn = document.querySelector("input");
+let select = document.querySelector("select");
+btn.addEventListener("click", onSubmit);
+function onSubmit(){
+    
+    let child = select.children;
+    for ( let i= 0; i < child.length; i++){
+        if(select.value === child[i].value){
+            child[i].remove();
+        }
+    }
+}
